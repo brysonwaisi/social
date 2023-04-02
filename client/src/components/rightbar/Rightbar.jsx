6a203cd697d9d546cc0068c0f139a0bab0b44c03
brysonwaisi <brysonnyamwange@gmail.com>
2023-04-02 22:13:05 +0300

    feat: left and right sidebar implementation

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,105 @@
 import './rightbar.scss'
-import React from 'react'
+import uno from '../../assets/uno.jpeg'
 
 const Rightbar = () => {
   return (
-    <div>Rightbar</div>
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Your Suggestions</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <span>Breezy</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <span>Breezy</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activity</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <p>
+                <span>Breezy</span> changed their profile pic
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <p>
+                <span>Breezy</span> changed their profile pic
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <p>
+                <span>Breezy</span> changed their profile pic
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <p>
+                <span>Breezy</span> changed their profile pic
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <div className="online" />
+              <span>Breezy</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <div className="online" />
+              <span>Breezy</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <div className="online" />
+              <span>Breezy</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src={uno} alt="" />
+              <div className="online" />
+              <span>Breezy</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
